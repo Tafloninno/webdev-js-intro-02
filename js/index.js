210b747dca7c999921db5ccc5b3c5144632ee536
Mugisha Innocent <111200646+Tafloninno@users.noreply.github.com>
2024-12-19 17:14:50 -0500

    experiment in index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,22 @@
-// Prevent us from attempting to use variables
-// that are not declared
-"use strict"
+// // Prevent us from attempting to use variables
+// // that are not declared
+// "use strict"
+
+// const myName = "Innocent";
+// console.log(myName)             // Output: "Innocent"
+
+
+// const number = 1;
+// console.log(number)                // Output: 1
+
+
+// let student = true;            
+// console.log(student)           // Output: true
+
+
+// student = false;
+
+// console.log(student)           // Output: false
+
+
 
